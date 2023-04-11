50232745094ea3d92d7c965aecffdd42a9c7d649
sladeisnub <130238936+sladeisnub@users.noreply.github.com>
2023-04-11 17:04:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.BotName = "Miku"
+global.Owner = ['919365986273']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['919365986273']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['919365986273']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.BotName = "SLADE"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.OwnerName = "Slade"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = [~_~] // Prefix ---  change it to anythong you want except '@' //
 
 
 global.location = "West Bengal, India"
